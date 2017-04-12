b444c0e87827e38f3b31ae1ab071f50f30efae39
seannicholson <snicholson@cloudpassage.com>
2017-04-11 20:29:57 -0400

    v1.1
    added error checking for 502 and 504 errors

--- README.md
+++ README.md
@@ -17,6 +17,9 @@ This script requires Python 2.7.10 or greater
 This script requires the CloudPassage Python SDK
 > pip install cloudpassage
 
+This script requires the Requests Python module.
+>pip install requests
+
 Install from pip with pip install cloudpassage. If you want to make modifications to the SDK you can install it in editable mode by downloading the source from this github repo, navigating to the top directory within the archive and running pip install -e . (note the . at the end). Or you can visit https://github.com/cloudpassage/cloudpassage-halo-python-sdk to clone it directly from our github.
 
 # Dependencies 
--- halo_sva_report.py
+++ halo_sva_report.py
@@ -1,16 +1,22 @@
 # WARNING: This script takes a long time to execute if you have a high count
 #          of active servers.
 # Author: Sean Nicholson
-# Version 1.0
+# Version 1.1
 # Date 04.07.2017
 ##############################################################################
 
 # Import Python Modules
-import json, csv
+import json, csv, base64, requests
 import cloudpassage
 import yaml
 import time
+from time import sleep
 global api_session
+
+# Set variable types
+user_credential_b64 = ''
+headers = {}
+api_key_description = ''
 
 
 # Define Methods
@@ -31,34 +37,81 @@ def byteify(input):
     else:
         return input
 
-# 
+def get_headers():
+    # Create headers
+    with open('cloudpassage.yml') as config_settings:
+        api_info = yaml.load(config_settings)
+        api_key_token = api_info['defaults']['key_id'] + ":" + api_info['defaults']['secret_key']
+        api_request_url = "https://" + api_info['defaults']['api_hostname'] + ":443"
+    user_credential_b64 = "Basic " + base64.b64encode(api_key_token)
+    reply = get_access_token(api_request_url, "/oauth/access_token?grant_type=client_credentials",
+                             {"Authorization": user_credential_b64})
+    reply_clean = reply.encode('utf-8')
+    headers = {"Content-type": "application/json", "Authorization": "Bearer " + reply_clean}
+    #print headers
+    return headers
+
+# Request Bearer token and return access_token
+def get_access_token(url, query_string, headers):
+    retry_loop_counter = 0
+    while retry_loop_counter < 5:
+        reply = requests.post(url + query_string, headers=headers)
+        #print reply.status_code
+        if reply.status_code == 200:
+            return reply.json()["access_token"]
+            retry_loop_counter = 10
+        else:
+            retry_loop_counter += 1
+            time.sleep(30)
+
+#
 def get_scan_data(session):
-        out_file = "Vunerability_Report_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
-        ofile  = open(out_file, "w")
-        halo_server_list = get_halo_servers_id(session)
-        get_halo_servers_scans= cloudpassage.HttpHelper(session)
-        ofile.write('AWS Account Number,AWS Instance ID,Package Name,Package Version,CVE,CVE Rating,CVE Information\n')
-        server_count = 1
-        total_servers = len(halo_server_list)
-        for server in halo_server_list:
-            #print server['halo_server_id']
-            print "Processing {0} of {1} servers".format(server_count, total_servers)
-            server_count+=1
-            api_url = '/v1/servers/' + server['halo_server_id'] + '/svm'
-            current_server=get_halo_servers_scans.get(api_url)
-            #print current_server
-            if 'scan' in current_server:
-                current_findings = current_server['scan']['findings']
-                #print current_findings
-                for finding in current_findings:
-                    if finding['status'] == 'bad':
-                        finding_cves = finding['cve_entries']
-                        for cve in finding_cves:
-                            if float(cve['cvss_score']) >= 7.0:
-                                cve_link="https://cve.mitre.org/cgi-bin/cvename.cgi?name=" + cve['cve_entry']
-                                row="'{0}',{1},{2},{3},{4},{5},{6}\n".format(server['aws_account_id'],server['aws_instance_id'],finding['package_name'],finding['package_version'],cve['cve_entry'],'High',cve_link)
-                                ofile.write(row)
-        ofile.close()
+    headers = get_headers()
+    out_file = "Vunerability_Report_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
+    ofile  = open(out_file, "w")
+    halo_server_list = get_halo_servers_id(session)
+    get_halo_servers_scans= cloudpassage.HttpHelper(session)
+    ofile.write('AWS Account Number,AWS Instance ID,Package Name,Package Version,CVE,CVE Rating,CVE Information\n')
+    server_count = 1
+    total_servers = len(halo_server_list)
+    for server in halo_server_list:
+        retry_loop_counter = 0
+        #print server['halo_server_id']
+        print "Processing {0} of {1} servers".format(server_count, total_servers)
+        server_count+=1
+        api_url = '/v1/servers/' + server['halo_server_id'] + '/svm'
+        requests_url = 'https://api.cloudpassage.com:443/v1/servers/' + server['halo_server_id'] + '/svm'
+        #print requests_url
+        #current_server=get_halo_servers_scans.get(api_url)
+        while retry_loop_counter < 5:
+            data = requests.request("GET", requests_url, data=None, headers=headers)
+            status_code = str(data.status_code)
+            #print status_code
+            if status_code == "200":
+                data = data.json()
+                #print data
+                #print current_server
+                if 'scan' in data:
+                    current_findings = data['scan']['findings']
+                    #print current_findings
+                    for finding in current_findings:
+                        if finding['status'] == 'bad':
+                            finding_cves = finding['cve_entries']
+                            for cve in finding_cves:
+                                if float(cve['cvss_score']) >= 7.0:
+                                    cve_link="https://cve.mitre.org/cgi-bin/cvename.cgi?name=" + cve['cve_entry']
+                                    #print cve['cve_entry']
+                                    row="'{0}',{1},{2},{3},{4},{5},{6}\n".format(server['aws_account_id'],server['aws_instance_id'],finding['package_name'],finding['package_version'],cve['cve_entry'],'High',cve_link)
+                                    ofile.write(row)
+                retry_loop_counter = 6
+            elif status_code == "401":
+                headers = get_headers()
+                retry_loop_counter += 1
+            elif status_code:
+                print "Response Error:{0}, pausing 60 secs, then retrying".format(status_code)
+                retry_loop_counter += 1
+                time.sleep(60)
+    ofile.close()
 
 
 
